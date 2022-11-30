8a7fa8b4f084297c763af89e8400c65452435c65
GautamaGandhi <gaga4723@colorado.edu>
2022-11-30 15:06:17 -0700

    Updated application-code.mk file

--- base_external/package/application-code/application-code.mk
+++ base_external/package/application-code/application-code.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-APPLICATION_CODE_VERSION = '893757270a8865729bd0646072263bc90a1e2fb0'
+APPLICATION_CODE_VERSION = 'b4c59671afb0274463387064e504d43ab0e71d8a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,13 +18,16 @@ APPLICATION_CODE_GIT_SUBMODULES = YES
 define APPLICATION_CODE_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio_test/libgpiod all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio_test/pigpio all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client-render/rpi_ws281x all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/capture_server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define APPLICATION_CODE_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/gpio_test/pigpio/pigpio-test $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/gpio_test/libgpiod/libgpiod-test $(TARGET_DIR)/usr/bin/
-
+	$(INSTALL) -m 0755 $(@D)/client-render/rpi_ws281x/test $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/capture_server/server $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
